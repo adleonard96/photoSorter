62edb37137643af0832d1aa94bf418774676b017
Alexander Leonard <71717066+adleonard96@users.noreply.github.com>
2024-09-27 21:10:53 -0400

    added docs

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const Persistance = require('./Persistance');
 const SortOption = require('./SortOption');
 
 let win;
+
+/**
+ * creates main window
+ */
 function createWindow() {
     win = new BrowserWindow({
         width: 800,
@@ -18,11 +22,17 @@ function createWindow() {
 }
 
 
+/**
+ * open file select dialog
+ */
 ipcMain.handle('file:open', async () => {
     let handler = new fileHandler();
     return await handler.openDialog();
 })
 
+/**
+ * retrieves first photo 
+ */
 ipcMain.handle('file:firstPhoto', async (_, directory) => {
     let handler = new fileHandler();
     let lastPosition = await Persistance.checkPosition(directory[0]);
@@ -30,21 +40,32 @@ ipcMain.handle('file:firstPhoto', async (_, directory) => {
     return files[lastPosition];
 })
 
+/**
+ * gets next photo in the directory
+ */
 ipcMain.handle('file:nextPhoto', async () => {
     let handler = new fileHandler();
     return handler.getNextPhoto();
 })
 
+/**
+ * gets previous photo in the directory
+ */
 ipcMain.handle('file:previousPhoto', async () => {
     let handler = new fileHandler();
     return handler.getPreviousPhoto();
 })
 
+/**
+ * gets the folders in the currect directory
+ */
 ipcMain.handle('file:getCurrentFolders', () => {
     return fileHandler.getDirectoryFolders();
 })
 
-
+/**
+ * creates folder for photos to be sorted in, if directory doesn't already exist
+ */
 ipcMain.handle('file:AddSortOption', async (_, options) => {
     let [option, folder] = options;
     if(await fileHandler.checkForFolder(option, folder)){
@@ -53,6 +74,9 @@ ipcMain.handle('file:AddSortOption', async (_, options) => {
     await fileHandler.createFolder(option, folder);
 })
 
+/**
+ * makes a copy of the photo over to the new directory
+ */
 ipcMain.handle('file:copyPhoto', async (_, options) => {
     let [photoPath, rootPath, copyFolders] = options;
     for(let folder of copyFolders){
@@ -60,15 +84,24 @@ ipcMain.handle('file:copyPhoto', async (_, options) => {
     }
 })
 
+/**
+ * creates a sort option
+ */
 ipcMain.handle('element:sortOption', (_, inputVal) => {
     return SortOption.getSortOption(inputVal);
 })
 
+/**
+ * creates all sort options already in teh directory
+ */
 ipcMain.handle('element:existingSortOptions', () => {
     let options = fileHandler.getDirectoryFolders();
     return SortOption.getSortOptions(options);
 })
 
+/**
+ * creates window on create
+ */
 app.whenReady().then(() => {
     createWindow()
 
@@ -79,6 +112,7 @@ app.whenReady().then(() => {
     })
 })
 
+/** handels close and saves last position on shutdown */
 app.on('window-all-closed', async () => {
     if (process.platform !== 'darwin') {
         await Persistance.savePosition();
